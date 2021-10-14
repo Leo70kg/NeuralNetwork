bd118471ea45a29fe12349e90a809e8aa14653f5
Leo70kg <zuoyou1993@hotmail.com>
2021-10-15 00:19:32 +0100

    resolve the problem of non convergence

---
+++ HJBLQ.cpp
@@ -0,0 +1,2 @@
+#include "HJBLQ.h"
+
---
+++ OneHotVector.h
@@ -0,0 +1,29 @@
+#ifndef __ONEHOTVECTOR_H__
+#define __ONEHOTVECTOR_H__
+class OneHotVector
+{
+public:
+    OneHotVector() = default;
+    OneHotVector(size_t index, size_t len) : m_index(index), m_Len(len) {}
+    virtual ~OneHotVector() {}
+    size_t Size() const
+    {
+        return this->m_Len;
+    }
+    size_t HotIndex() const
+    {
+        return m_index;
+    }
+
+    template<class T>
+    friend class Vector;
+
+    template<class T>
+    friend class Matrix;
+
+private:
+    size_t m_index;
+    size_t m_Len;
+};
+
+#endif
