132e6a200a716f7b1c57f7b4806342fc1fc1bdd3
Leo70kg <zuoyou1993@hotmail.com>
2021-10-14 23:50:23 +0100

    resolve the problem of non convergence

---
+++ BSDEConfiguration.h
@@ -0,0 +1,41 @@
+#ifndef __BSDECONFIGURATION_H__
+#define __BSDECONFIGURATION_H__
+
+#include <string>
+#include <vector>
+#include <memory>
+
+class BSDEModel;
+
+class BSDEConfiguration
+{
+public:
+    
+    BSDEConfiguration() = default;
+	static std::unique_ptr<BSDEConfiguration> CreateConfiguration(const std::string & filePath);
+    virtual bool Load(const std::string & filePath);
+    virtual bool Save(const std::string & filePath);
+    virtual std::unique_ptr<BSDEModel> CreateModel();
+    
+    int train_epoch;
+    float learning_rate;
+
+    // dimension of output value
+    int dim_output;
+
+    // dimension of input value
+    int dim_input;
+    int batchSize;
+    std::string modelSavePath;
+
+    float totalTime;
+    int numTimeInterval;
+    int subnetNumber;
+    int subnetLayerNumber;
+    int sampleSize;
+    std::vector<float> yInitRange;
+    std::vector<int> subnetHiddenLayerSizes;
+
+};
+
+#endif
---
+++ PricingDefaultRisk.h
@@ -0,0 +1,32 @@
+#ifndef __PRICINGDEFAULTRISK_H__
+#define __PRICINGDEFAULTRISK_H__
+
+#include "Vector.h"
+#include "Equation.h"
+
+class PricingDefaultRisk : public Equation
+{
+public:
+    PricingDefaultRisk() = default;
+    PricingDefaultRisk(const BSDEConfiguration& config);
+    ~PricingDefaultRisk();
+	float f_tf(float t, const Vector<float>&x, float y, const Vector<float>&z) const override;
+    float f_tf_diff_y(float y) const override;
+    float g_tf(float t, const Vector<float>&x) const override;
+    bool XSample() override;
+    bool DwSample() override;
+
+private:
+    float sigma;
+    float rate;
+    float delta;
+    float gammah;
+    float gammal;
+    float mu_bar;
+    float vh;
+    float vl;
+    float slope;
+
+};
+
+#endif
